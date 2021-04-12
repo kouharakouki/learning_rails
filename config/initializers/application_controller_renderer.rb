8aaa474025c919a40c1fb596350d2361106edfa6
kouharakouki <koki.kohara916@gmail.com>
2021-04-12 02:41:28 +0000

    Finish ~Chapter10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ba3d32a7238a46b7630dc4f492b555d0a760c62acabd422561fd191b59800464e800eaf9616b98b58b64cdcc01337fbd9a968497410678a2ddff0de6a8a07d0d'
